c9226903e44b06e6aaa0857cea9c9abd2ddd2556
ThomasChun <tchun2055@gmail.com>
2018-12-12 07:09:06 -1000

    started array class

--- DSA-Arrays-mary-thomas.js
+++ DSA-Arrays-mary-thomas.js
@@ -1,3 +1,16 @@
 import memory from './memory';
 
- 
+class Array {
+  constructor() {
+    this.length = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    this._resize(this.length + 1);
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  
+}
